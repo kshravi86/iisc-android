0594bd4c1cb0c0c0f6ca71feb293ff1b225a5073
aakashravi <aakashravi@gnani.ai>
2020-03-27 16:59:36 +0530

    added

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# iisc-android
+# Registration-form
+
+1.use android and java for this registration form application.
+
+
+
+2.I use android studio for develop this basic application.
+
+
+
+
+3.Also use Strong Password :
+
+
+ Strong Password- 
+ 
+                  It contains at least one digit.
+                  
+                  It contains at least one lowercase English character.
+                  
+                  It contains at least one uppercase English character.
+                  
+                  It contains at least one special character. The special characters are: !@#$%^&*()-+.
+
---
+++ app/src/main/java/test1/pantha/registrationform/MainActivity.java
@@ -0,0 +1,29 @@
+package test1.pantha.registrationform;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+   Button button1,button2,button3;
+
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       ;
+
+
+
+    }
+}
